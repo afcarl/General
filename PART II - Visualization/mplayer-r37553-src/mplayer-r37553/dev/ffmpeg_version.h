9a9462edb3773c8b2ac4c2ff775d5f6c0e6a6670
Priya <pnk230@nyu.edu>
2015-11-23 08:22:25 -0500

    Started 3D Visualization

---
+++ PART II - Visualization/mplayer-r37553-src/mplayer-r37553/dev/ffmpeg_version.h
@@ -0,0 +1,8 @@
+#define FFMPEG_VERSION "N-76684-g1fe82ab"
+#define FFMPEG_GIT_FULL "1fe82abac7ecc072b83644dcd89678b50d69d23a"
+#define FFMPEG_GIT_SHORT "1fe82ab"
+
+#define CHECKOUT_DATE "Sun, Nov 15, 2015 22:38:39"
+#define LAST_COMMIT_DATE "Mon Nov 16 01:53:47 2015 +0100"
+#define LAST_COMMIT_TITLE "ffmpeg: Simplify fps code related to delta0"
+#define DETACHED_HEAD "0"
