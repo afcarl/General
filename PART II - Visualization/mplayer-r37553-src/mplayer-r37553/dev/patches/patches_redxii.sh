e59072ea1641e8cac27a95ddd5501d300ebf9f00
Priya <pnk230@nyu.edu>
2015-11-23 08:26:15 -0500

    Started 3D Visualization- corrections

--- PART II - Visualization/mplayer-r37553-src/mplayer-r37553/dev/patches/patches_redxii.sh
+++
@@ -1,4 +0,0 @@
-for p in $(pwd)/redxii/*.diff; do
-  echo "- Applying $(basename $p)"
-  patch -d ../.. -p0 < $p || return 1
-done
--- PART II - Visualization/mplayer-r37553-src/mplayer-r37553/dev/patches/patches_sherpya.sh
+++
@@ -1,4 +0,0 @@
-for p in $(pwd)/sherpya/*; do
-  echo "- Applying $(basename $p)"
-  patch -d ../.. -p1 < $p || return 1
-done
