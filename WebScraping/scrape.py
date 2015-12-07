bc4f000af6b19474af8f52cf0177504e97353a65
priyakhokher <pnk230@nyu.edu>
2015-12-07 02:36:40 -0800

    Started Webscraping-thanks to my adorable Telugu guys-Vamsi and Adi, also adding the first manual dataset collected for ADS Project-with Nikhil

---
+++ WebScraping/scrape.py
@@ -0,0 +1,13 @@
+import requests
+from BeautifulSoup import BeautifulSoup
+
+url = 'http://www.showmeboone.com/sheriff/JailResidents/JailResidents.asp'
+response = requests.get(url)
+html = response.content
+
+soup = BeautifulSoup(html)
+table = soup.find('table', attrs={'class': 'resultsTable'})
+
+for row in table.findAll('tr'):
+    for cell in row.findAll('td'):
+        print cell.text.replace('&nbsp;', '')
