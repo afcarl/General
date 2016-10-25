5aedb1a47d63082cf7fcfb81040e5ce51ad60c6e
priyakhokher <pnk230@nyu.edu>
2016-10-24 22:59:39 -0700

    reverse string

--- DSA/stacks_1.py
+++ DSA/stacks_1.py
@@ -2,28 +2,33 @@ from collections import deque
 
 class stacks(object):
 	def __init__(self,stack_size):
+		# initialize stack with size
 		self.stack_size = stack_size
 		self.d = deque(maxlen = self.stack_size)
 
 	def pop(self):
-		
+		# check size of stack so that if it's - there is nothing to pop
 		if len(self.d)==0:
-			print "no value to pop"
+
+			return 'error'
 		else:
 
-			self.d.pop()
-			print self.d
+			return self.d.pop()
+			#print self.d
 
 	def push(self,value):
+		# check if the stack has space to push value
 		if self.isEmpty():
 			self.d.append(value)
-			print self.d
+			#print self.d
 			
 		else:
 			print "stack is full"
 			
 
 	def isEmpty(self):
+		# is empty - there should be space for atleast one value 
+		# else stack is full
 		if len(self.d) < self.stack_size:
 			return True
 
@@ -31,22 +36,38 @@ class stacks(object):
 			return False
 
 	def top(self):
+		# return the top values
 		if len(self.d) == 0:
-			print "no value to show"
+			return "none"
 
 		else:
-			print self.d[-1]
+			return self.d[-1]
 			
 
 
 
 if __name__ == '__main__':
-	s = stacks(4)
-	s.pop()
-	s.push(3)
-	s.push(1)
-	s.push(3)
-	s.push(2)
-	s.push(9)
-	s.top()
-	s.pop()
+	# Uncomment below line for just playing with stacks
+	# s = stacks(4)
+	# s.pop()
+	# s.push(3)
+	# s.push(1)
+	# s.push(3)
+	# s.push(2)
+	# s.push(9)
+	# s.top()
+	# s.pop()
+
+	# Reverse a string
+
+	input_string = "priya"
+	s = stacks(len(input_string))
+	for i in input_string:
+		s.push(i)
+	reversed_string = []
+	while(s.top()!="none"):
+		
+		reversed_string.append(s.pop())
+		
+	print ''.join(reversed_string)
+
