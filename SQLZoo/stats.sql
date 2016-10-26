01efc9ab8045540e4c6dfa4e87e03e6486ea884f
priyakhokher <pnk230@nyu.edu>
2016-10-25 21:33:12 -0700

    running median

--- SQLZoo/stats.sql
+++ SQLZoo/stats.sql
@@ -6,4 +6,10 @@ order by count(*)
 -- median 
 select * from (select count(*) rank,w.name from world w join world w1 where w.population<=w1.population 
 group by w.name
-order by count(*)) new_table where rank = (select floor(count(*)/2) from world); 
+order by count(*)) new_table where rank = (select floor(count(*)/2) from world);
+
+-- running total
+select count(*),sum(w1.population),w.name,w.population 
+from world w join world w1 where w.population<=w1.population 
+group by w.name,w.population 
+order by count(*);
