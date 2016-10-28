5afc04e9e375cdbcd9daeeb56a3d2c3973f7e1f6
priyakhokher <pnk230@nyu.edu>
2016-10-27 22:10:32 -0700

    Linked_list basics in Python

--- DSA/balanced_parenthesis.py
+++ DSA/balanced_parenthesis.py
@@ -7,14 +7,17 @@ closing_list = list(")}]")
 d = deque(maxlen = len(input_string))
 
 input_string_filter = filter(lambda x:x in list("({[]})"), input_string)
+# use the above string in place of input_string below when position of error is not required
 k = 0
 for i in input_string:
 	if i in opening_list:
-		d.append(i)
+		d.append(i) # add to stack the opening bracket
 
 	elif i in closing_list:
 		
 		if opening_list.index(d[-1]) == closing_list.index(i):
+			# the current closing bracket should be equal to the top of the stack which contains the latest opening bracket
+			# if yes then pop the top from stack
 			d.pop()
 		else:
 			print "unbalanced paranthesis at position"
