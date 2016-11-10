97507154630ff1b930dd516f531accfef36239d1
priyakhokher <pnk230@nyu.edu>
2016-11-09 20:46:54 -0800

    insertion sort revised

--- DSA/Sorting/insertion_sort.py
+++ DSA/Sorting/insertion_sort.py
@@ -8,17 +8,18 @@ def insertion_sort(seq):
 	'''
 	n = len(seq)
 	for i in range(1,n):
-		j = i
-		k = i-1
+		# Start from index one in the right unsorted list for comparision with the left sorted list, 
+		#  As the left sorted list begins with one element in it which is always sorted
+		j = i # index of the new number to be inserted
+		k = i-1 # Immediate left value to the new "to be inserted element"
 		while j>0 and k>=0:
+			# j will go minimum to 1 and k to 0 as its the value in the left sorted value
 			if seq[j]<seq[k]:
-				seq[k],seq[j] = seq[j],seq[k]
+				seq[k],seq[j] = seq[j],seq[k] # swap the values
 				j = k
 				k = k-1
 			else:
 				break
-
-				j = j-1
 
 	print seq
 
